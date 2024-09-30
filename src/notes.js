74bebcdf95709d4b06783ad1e69aacc0bdd3db4f
Samuel Ezra Nathan Andrew Sirait <samuelezra2013@gmail.com>
2024-09-30 12:17:04 +0800

    saving the note

---
+++ src/notes.js
@@ -0,0 +1,16 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+ 
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+ 
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+ 
+  const newNote = {
+   title, tags, body, id, createdAt, updatedAt,
+  };
+ 
+  notes.push(newNote);
+};
